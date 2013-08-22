e6547078e52b626c2bba703823fbdc9b9e11d077
joao paulo <jpcamargo+github@gmail.com>
2013-08-22 11:43:30 -0300

    minor changes in the install.sh script

--- install.sh
+++ install.sh
@@ -3,26 +3,18 @@
 DOTFILES=`pwd`
 DOTFILES_DIR=`echo $DOTFILES | sed "s|$HOME/||"`
 
-# reading the profile
-source ~/.bash_profile
+# update the repo
+if [ -d ".git" ]; then
+  git pull origin master;
+  git submodule update --init;
+fi
 
-# Files to link
-# .bash_profile
-# .bash_functions
-# .gemrc
-# .gitconfig
-# .gitignore_global
-# .aprc
-# .rvmrc
-# .ackrc
-# .gvimrc
-# .vim
-# .vimrc
-# .fonts
+# reading the profile
+[ -f '~/.bash_profile' ] && source ~/.bash_profile;
 
 cd $HOME
 
-for FILE in "bash_profile" "bash_functions" "gemrc" "gitconfig" "gitignore_global" "aprc" "rvmrc" "ackrc" "vim/gvimrc" "vim" "vim/vimrc" "vim/fonts"
+for FILE in "bash_profile" "bash_functions" "gemrc" "gitconfig" "gitignore_global" "aprc" "rvmrc" "ackrc" "vim" "vim/gvimrc" "vim/vimrc" "fonts"
 do
   # linking project file, if exists
   if [ -f $DOTFILES_DIR/$FILE ]; then
@@ -33,7 +25,6 @@ done
 
 # Vim
 mkdir -p ~/.vim/tmp ~/.vim/swap;
-git submodule update --init;
 
 vim +BundleInstall +qall
 
