805ef98cba76fdbe71ce2cd4076292016ee55d50
joao paulo <jpcamargo+github@gmail.com>
2013-11-28 15:46:28 -0200

    adds a retry feature to the curl command

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -3,10 +3,11 @@
 
 function countdown() {
   seconds=$1
+  message=$2
   while [[ $seconds -gt 0 ]]; do
     seconds=$(expr $seconds - 1)
     sleep 1
-    printf "\r Time until next iteration: \033[1m%02d:%02d:%02d\033[0m" \
+    printf "\r $message: \033[1m%02d:%02d:%02d\033[0m" \
                         $((seconds/3600)) $(( (seconds/60)%60)) $((seconds%60))
   done
   printf "\r                           "
@@ -15,22 +16,26 @@ function countdown() {
 
 echo -e "\033[0;30;42m Starting script at \033[1;30m`date "+%H:%M:%S %d/%m/%Y"`                                   \033[0m"
 
-CURRENT_DATE=`date "+%Y-%m-%d_%H-%M-%s"`
+CURRENT_DATE=`date "+%Y-%m-%d_%H-%M-%S"`
 
 while [[ "$CURRENT_DATE" < "2013-11-30_00-00-00" ]]; do
-  echo " Saving images... `date "+%H:%M:%S %d/%m/%Y"`"
+  echo " Saving images..."
   for website in submarino magazineluiza buscape americanas walmart; do
     echo "   saving ${website}'s screenshot..."
-    filename="image_${website}_$CURRENT_DATE.png"
+    filename="image_${website}_${CURRENT_DATE}.png"
 
-    NEXT_WAIT_TIME=0
+    RETRIES=0
     COMMAND_STATUS=1
-    until [[ $COMMAND_STATUS -eq 0 || $NEXT_WAIT_TIME -eq 4 ]]; do
+    until [[ $COMMAND_STATUS -eq 0 || $RETRIES -eq 5 ]]; do
+
+      [[ $RETRIES -lt 0 ]] && echo -e " \033[31mAn error has occurred, retrying...\033[0m"
       curl -s -L -D /tmp/headers "http://api.snapito.com/?url=http://${website}.com.br&type=png&um=box&freshness=1&delay=-1&filename=${filename}" -o $filename
 
       COMMAND_STATUS=$?
-      sleep $NEXT_WAIT_TIME
-      let NEXT_WAIT_TIME=NEXT_WAIT_TIME+1
+      if [[ $COMMAND_STATUS -ne 0 ]]; then
+        countdown 5 "Retrying in"
+      fi
+      let RETRIES=$((RETRIES++))
     done
 
     if [ -e $filename ]; then
@@ -44,7 +49,7 @@ while [[ "$CURRENT_DATE" < "2013-11-30_00-00-00" ]]; do
   endTime=$(date -v+1H -v0M -v0S +%s)
   timeToWait=$(($endTime - $startTime))
 
-  countdown $timeToWait
+  countdown $timeToWait "Time until next iteration"
 
   CURRENT_DATE=`date "+%Y-%m-%d_%H-%M-%s"`
 done
