6935c842d0a41a3b01e26e888af5ab5bdb349c36
joao paulo <jpcamargo+github@gmail.com>
2013-11-28 15:21:00 -0200

    hourly screenshot taker

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # javascript:var unencodedURL = (location.href); var websiteURL = encodeURIComponent(unencodedURL); var url = 'http://api.snapito.com/?url=' + websiteURL + '&type=png&um=box&freshness=1&filename=image.png'; window.open(url, 'Download', '')
 
 function countdown() {
@@ -7,21 +6,38 @@ function countdown() {
   while [[ $seconds -gt 0 ]]; do
     seconds=$(expr $seconds - 1)
     sleep 1
-    printf "\rTime until next iteration: %02d:%02d:%02d" \
+    printf "\r Time until next iteration: \033[1m%02d:%02d:%02d\033[0m" \
                         $((seconds/3600)) $(( (seconds/60)%60)) $((seconds%60))
   done
   printf "\r                           "
   echo ""
 }
 
+echo -e "\033[0;30;42m Starting script at \033[1;30m`date "+%H:%M:%S %d/%m/%Y"`                                   \033[0m"
+
 CURRENT_DATE=`date "+%Y-%m-%d_%H-%M-%s"`
 
 while [[ "$CURRENT_DATE" < "2013-11-30_00-00-00" ]]; do
-  echo "Saving images... `date "+%Y-%m-%d %H:%M:%s"`"
-  for website in submarino magazineluiza; do
-    echo "  Saving $website's screenshot..."
-    curl -s -L -D /tmp/headers "http://api.snapito.com/?url=http://${website}.com.br&type=png&um=box&freshness=1&filename=image_${website}_${CURRENT_DATE}.png" -o image_${website}_$CURRENT_DATE.png
-    echo "    - image_${website}_$CURRENT_DATE.png written."
+  echo " Saving images... `date "+%H:%M:%S %d/%m/%Y"`"
+  for website in submarino magazineluiza buscape americanas walmart; do
+    echo "   saving ${website}'s screenshot..."
+    filename="image_${website}_$CURRENT_DATE.png"
+
+    NEXT_WAIT_TIME=0
+    COMMAND_STATUS=1
+    until [[ $COMMAND_STATUS -eq 0 || $NEXT_WAIT_TIME -eq 4 ]]; do
+      curl -s -L -D /tmp/headers "http://api.snapito.com/?url=http://${website}.com.br&type=png&um=box&freshness=1&delay=-1&filename=${filename}" -o $filename
+
+      COMMAND_STATUS=$?
+      sleep $NEXT_WAIT_TIME
+      let NEXT_WAIT_TIME=NEXT_WAIT_TIME+1
+    done
+
+    if [ -e $filename ]; then
+      echo -e "   \033[0;32m- ${filename} written.\033[0m"
+    else
+      echo -e "   \033[0;31m- ${filename} was not written.\033[0m"
+    fi
   done
 
   startTime=$(date +%s)
@@ -32,3 +48,6 @@ while [[ "$CURRENT_DATE" < "2013-11-30_00-00-00" ]]; do
 
   CURRENT_DATE=`date "+%Y-%m-%d_%H-%M-%s"`
 done
+
+echo -e "\033[0;32mAll done at\033[0m `date "+%H:%M:%S %d/%m/%Y"`, \033[0,31mcongratulations!\033[0m"
+
