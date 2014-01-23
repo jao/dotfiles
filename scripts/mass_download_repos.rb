4d0a4f92fb3eb4723756567ba9bfcdc172bc9fc5
joao paulo <jpcamargo+github@gmail.com>
2014-01-23 16:10:13 -0200

    minor changes to the mass clone repos script

--- scripts/mass_download_repos.rb
+++ scripts/mass_download_repos.rb
@@ -8,7 +8,7 @@ require 'open-uri'
 require 'yaml'
 
 
-agent = Mechanize.new { |a| a.log = Logger.new(STDERR) }
+agent = Mechanize.new
 
 page = agent.get("https://code.locaweb.com.br/users/sign_in")
 
@@ -20,22 +20,22 @@ form.username = config['auth']['username']
 form.password = config['auth']['password']
 page = form.submit form.buttons.first
 
-['saas','paas'].each do |group|
-  puts "\e[1;30;43m Reading repos for group: " + group + " \e[0m"
+config['groups'].each do |group|
+  puts "\e[30;43m Reading repos for group: " + group + " \e[0m"
   group_page = agent.get("https://code.locaweb.com.br/groups/#{group}")
 
   doc = Nokogiri::HTML(group_page.body)
 
   doc.xpath('//ul[@class="well-list"]/li[@class="project-row"]/a').each do | link |
-    url = link.attributes['href'].to_s
-    puts "\e[33m  " + url.gsub!(/^\//,'') + " \e[0m"
-
+    url = link.attributes['href'].to_s.gsub(/^\//,'')
     base_dir, repo = url.split('/')
+    puts "\e[33m  " + repo + " \e[0m"
 
     # create the base dir if necessary
     FileUtils.mkdir(base_dir) unless File.directory?(base_dir) || File.directory?(group)
 
     # clone the repo
-    puts `git clone git@code.locaweb.com.br:#{url}.git #{url}` unless File.directory?(url)
+    `git clone git@code.locaweb.com.br:#{url}.git #{url}` unless File.directory?(url)
   end
+  puts
 end
