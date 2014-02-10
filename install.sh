1c60da81c507c6a0fcef71168793590228863c9a
joao paulo <jpcamargo+github@gmail.com>
2014-02-09 23:30:37 -0200

    missing a file in the install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ fi
 
 cd $HOME
 
-for FILE in "ack_completion" "bash_profile" "bash_functions" "gemrc" "gitconfig" "gitignore_global" "aprc" "rvmrc" "ackrc" "vim" "vim/gvimrc" "vim/vimrc" "fonts"
+for FILE in "ack_completion" "bash_profile" "bash_functions" "gemrc" "gitconfig" "gitignore_global" "aprc" "rvmrc" "ackrc" "vim" "vim/gvimrc" "vim/vimrc" "fonts" "rspec"
 do
   # linking project file, if exists
   if [ -f $DOTFILES_DIR/$FILE ] || [ -d $DOTFILES_DIR/$FILE ]; then
